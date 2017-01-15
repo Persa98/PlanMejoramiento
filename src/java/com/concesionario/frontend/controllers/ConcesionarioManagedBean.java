b2e7c4df52c49b447bcc20e4d73122c92e7f3d57
Persa98 <kamilomomtenegro98@gmail.com>
2017-01-15 12:40:23 -0500

    Correccion 2.0

--- src/java/com/concesionario/frontend/controllers/ConcesionarioManagedBean.java
+++ src/java/com/concesionario/frontend/controllers/ConcesionarioManagedBean.java
@@ -5,9 +5,15 @@
  */
 package com.concesionario.frontend.controllers;
 
+import com.concesionario.backend.persistence.entities.Concesionario;
+import com.concesionario.backend.persistence.facade.ConcesionarioFacadeLocal;
+import com.concesionario.frontend.util.Managedbean;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 
 /**
  *
@@ -15,12 +21,35 @@ import java.io.Serializable;
  */
 @Named(value = "concesionarioManagedBean")
 @SessionScoped
-public class ConcesionarioManagedBean implements Serializable {
+public class ConcesionarioManagedBean implements Serializable, Managedbean <Concesionario> {
 
-    /**
-     * Creates a new instance of ConcesionarioManagedBean
-     */
+    private Concesionario conces;
+    @EJB
+    private ConcesionarioFacadeLocal cofl;
+    
     public ConcesionarioManagedBean() {
+    }
+
+    public Concesionario getConces() {
+        return conces;
+    }
+
+    public void setConces(Concesionario conces) {
+        this.conces = conces;
+    }
+    
+    @PostConstruct
+    public void init(){
+        conces = new Concesionario();
+    }
+    
+    public List<Concesionario> listarConcesionario(){
+        return cofl.findAll();
+    }
+
+    @Override
+    public Concesionario getObeject(Integer i) {
+        return cofl.find(i);
     }
     
 }
